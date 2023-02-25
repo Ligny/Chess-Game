ab3d1654a33a9af0d4558ce72c73d261949e3367
Ligny <antoine.deligny@epitech.eu>
2023-02-25 21:24:27 +0900

    ADD props to know case

--- Selection.py
+++ Selection.py
@@ -1,11 +1,12 @@
 class Selection:
-  def __init__(self, x: int, y: int) -> None:
+  def __init__(self, x: int, y: int, case: str) -> None:
     self._x = x
     self._y = y
     self._isCorrect = False
+    self._caseSelected: str = case
 
-  def verifySelection(self, isWhiteTurn: bool, map) -> bool:
+  def verifySelection(self, isWhiteTurn: bool) -> bool:
     if self._isCorrect == False and self._x >= 0 and self._x <= 7 and self._y >= 0 and self._y <= 7:
-      if (isWhiteTurn and map[self._y][self._x][0] == "w") or (not isWhiteTurn and map[self._y][self._x][0] == "b"):
+      if (isWhiteTurn and self._caseSelected[0] == "w") or (not isWhiteTurn and self._caseSelected[0] == "b"):
         self._isCorrect = True
     return self._isCorrect
